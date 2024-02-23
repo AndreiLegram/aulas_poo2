1c2b5c2ae9e9110848202ee8210c11db1caff4b2
Bernardo Marquetti <bernardo.060404@bento.local>
2024-02-22 21:20:36 -0300

    uns exercicios

---
+++ 1172.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+for (var i = 0 ; i < lines.length ; i++) {
+    if (lines[i] <= 0) {
+        lines[i] = 1
+    }
+    console.log('X['+i+'] = '+ Number(lines[i]));
+}
---
+++ 1173.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+let valor = lines[0]
+let newValor = valor
+for (var i = 0 ; i < 10 ; i++) {
+    console.log('N['+i+'] = '+ Number(newValor));
+    newValor = newValor * 2;
+}
