cc56e296ac20eba02f3f0b470523b7fabac1dbc1
Andrei Legramante <57579770+AndreiLegram@users.noreply.github.com>
2024-02-23 19:20:00 -0300

    Update 1179.js

--- 1179.js
+++ 1179.js
@@ -1,54 +1,21 @@
-var input = require('fs').readFileSync('dev/stdin', 'utf8');
-var lines = input.split('\r\n');
-
-var pares = [];
-var impares = [];
-var ip = 0;
-var ii = 0;
-for (let i = 0; i < lines.length; i++) {
-    if (lines[i] % 2 == 0) {
-        if (ip == 5) {
-            ip = 0;
-        }
-        pares[ip] = lines[i];
-        ip++;
-    } else {
-        if (ii == 5) {
-            ii = 0;
-        }
-        impares[ii] = lines[i];
-        ii++
-    }
-    console.log('X[' + i + '] = ' + Number(lines[i]));
-}
-
-// ta todo cagado nao funciona etc
-
-
-
-
-const entrada = require('fs').readFileSync('/dev/stdin', 'utf8')
-  .split('\n')
-  .map(x => Number(x));
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
 
 const pares = [];
 const impares = [];
 
 for (let i = 0; i < 15; i++) {
-  const n = entrada.shift();
+  const n = Number(lines.shift());
 
   if (impares.length == 5) {
       imprimeNumeros(impares, false);
-  }
-  else if (pares.length == 5) {
+  } else if (pares.length == 5) {
       imprimeNumeros(pares, true);
   }
 
   if (n % 2 === 0) {
       pares.push(n);
-  }
-  else
-  {
+  } else {
       impares.push(n);
   }
 }
@@ -68,46 +35,3 @@ function imprimeNumeros(numeros, saoPares) {
 
   numeros.length = 0;
 }
-
-
-//esse da runtime error
-
-
-
-
-
-
-
-let imprime = (nome, vetor) => {
-    for (let i = 0; i < vetor.length; ++i) {
-        console.log(`${nome}[${i}] = ${vetor[i]}`);
-    }
-};
-
-let impar = [];
-let par = [];
-for (let i = 0; i < 15; ++i) {
-    let valor = parseInt(lines.shift());
-
-    if (Math.abs(valor) % 2) {
-        impar.push(valor);
-
-        if (impar.length === 5) {
-            imprime("impar", impar);
-            impar = [];
-        }
-    } else {
-        par.push(valor);
-
-        if (par.length === 5) {
-            imprime("par", par);
-            par = [];
-        }
-    }
-}
-
-imprime("impar", impar);
-imprime("par", par);
-
-
-//esse é de baitola
