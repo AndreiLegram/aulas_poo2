018686a331d6604de32200c48c10daedf2e34fca
PedritoFerrrari <105124188+PedritoFerrrari@users.noreply.github.com>
2024-02-23 19:15:30 -0300

    Update 1179.js

--- 1179.js
+++ 1179.js
@@ -71,3 +71,43 @@ function imprimeNumeros(numeros, saoPares) {
 
 
 //esse da runtime error
+
+
+
+
+
+
+
+let imprime = (nome, vetor) => {
+    for (let i = 0; i < vetor.length; ++i) {
+        console.log(`${nome}[${i}] = ${vetor[i]}`);
+    }
+};
+
+let impar = [];
+let par = [];
+for (let i = 0; i < 15; ++i) {
+    let valor = parseInt(lines.shift());
+
+    if (Math.abs(valor) % 2) {
+        impar.push(valor);
+
+        if (impar.length === 5) {
+            imprime("impar", impar);
+            impar = [];
+        }
+    } else {
+        par.push(valor);
+
+        if (par.length === 5) {
+            imprime("par", par);
+            par = [];
+        }
+    }
+}
+
+imprime("impar", impar);
+imprime("par", par);
+
+
+//esse é de baitola
