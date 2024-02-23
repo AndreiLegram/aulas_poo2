c4704b764107cb928d303354ff97330f037809b3
PedritoFerrrari <105124188+PedritoFerrrari@users.noreply.github.com>
2024-02-22 21:21:33 -0300

    Update 1175.js

--- 1175.js
+++ 1175.js
@@ -1,11 +1,24 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
-let newArray = [];
-for (let i = lines.length ; i === 0 ; i--) {
-    newArray.push(parseInt(lines[i]));
+let number = parseInt(lines.shift());
+let array = [];
+let temp;
+
+for(let i = 0;i <= 19; i++){
+  array.push(number);
+  number = parseInt(lines.shift());
 }
 
-for (let j = 0 ; j < newArray.length ; j++ ) {
-    console.log('N['+ i + '] =' + Number(newArray[i]));
+for(let i = 0, j = 19; i < 10; i++ ,j--){
+  temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
 }
+
+for(let i = 0;i < array.length; i++){
+  console.log(`N[${i}] = ${array[i]}`);
+}
+
+
+//agora funciona
