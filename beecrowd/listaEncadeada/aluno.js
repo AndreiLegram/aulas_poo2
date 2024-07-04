2ab9608c76b2a8965316fa2b01de122719db7657
Bernardo Marquetti <bernardo.060404@bento.local>
2024-07-04 19:57:40 -0300

    oi andrei

---
+++ beecrowd/listaEncadeada/aluno.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.Aluno = void 0;
+var Aluno = /** @class */ (function () {
+    function Aluno(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    return Aluno;
+}());
+exports.Aluno = Aluno;
