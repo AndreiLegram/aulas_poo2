0a17d466c0c448e3d01df15076ba2afc13c9eba6
Andrei Legramante <andrei.050103@bento.local>
2024-07-04 20:47:20 -0300

    add lista_dupla

--- lista_dupla/aluno.ts
+++ lista_dupla/aluno.ts
@@ -2,12 +2,15 @@ export class Aluno {
     private nome: string;
     private idade: number;
 
-    public constructor(nome : string , idade : number) {
+    public constructor(nome: string, idade: number) {
         this.nome = nome;
         this.idade = idade;
     }
 
     public getNome() {
         return this.nome;
+    }
+    public getIdade() {
+        return this.idade;
     }
 }
--- lista_dupla/lista_ligada.ts
+++ lista_dupla/lista_ligada.ts
@@ -1,3 +1,4 @@
+import { Aluno } from "./aluno";
 import { Celula } from "./celula";
 
 export class ListaLigada {
@@ -11,7 +12,7 @@ export class ListaLigada {
         this._totalDeElementos = 0;
     }
 
-    public adicionaNoComeco(elemento: Object): void {
+    public adicionaNoComeco(elemento: Aluno): void {
         let nova: Celula = new Celula(this._primeira, elemento);
         this._primeira = nova;
         if (this._totalDeElementos == 0) {
@@ -21,7 +22,7 @@ export class ListaLigada {
         this._totalDeElementos++;
     }
 
-    public adicionar(elemento: Object): void {
+    public adicionar(elemento: Aluno): void {
         if (this._totalDeElementos == 0) {
             this.adicionaNoComeco(elemento);
         } else {
@@ -41,12 +42,12 @@ export class ListaLigada {
         let atual: Celula = this._primeira;
         // Percorrendo até o penúltimo elemento.
         for (let i: number = 0; i < this._totalDeElementos - 1; i++) {
-            str += atual.getElemento();
+            str += atual.getElemento().getNome();
             str += ", ";
             atual = atual.getProxima();
         }
         // último elemento
-        str += atual.getElemento();
+        str += atual.getElemento().getNome();
         str += "]";
         return str;
     }
@@ -65,7 +66,7 @@ export class ListaLigada {
         return atual;
     }
 
-    public adiciona(posicao: number, elemento: Object): void {
+    public adiciona(posicao: number, elemento: Aluno): void {
         if (posicao == 0) { // No começo.
             this.adicionaNoComeco(elemento);
         } else if (posicao == this._totalDeElementos) { // No fim.
@@ -78,7 +79,7 @@ export class ListaLigada {
         }
     }
 
-    public pega(posicao: number): Object {
+    public pega(posicao: number): Aluno {
         return this.pegaCelula(posicao).getElemento();
     }
 
@@ -125,10 +126,10 @@ export class ListaLigada {
     }
 
 
-    public contem(Object: elemento): bool {
+    public contem(elemento: Aluno): boolean {
         let atual: Celula = this._primeira;
         while (atual != null) {
-            if (atual.getElemento().equals(elemento)) {
+            if (atual.getElemento() == elemento) {
                 return true;
             }
             atual = atual.getProxima();
